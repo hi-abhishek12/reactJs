1728a8de735d60b3842fd8192a127db0f53f7485
hi-abhishek12 <abhisahni2005@gmail.com>
2025-05-04 09:32:28 +0000

    context api todos

--- todoContextApi/src/App.jsx
+++ todoContextApi/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
 import './App.css'
-
+import { TodoProvider } from './contexts'
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos , setTodos] = useState([])
 
+  const addTodo = () =>{
+    
+  }
+
+  const updateTodo = () =>{
+    
+  }
+
+  const removeTodo = () =>{
+    
+  }
+
+  const toogleComplete = () =>{
+    
+  }
   return (
-    <h1 class="text-4xl font-bold text-green-500">Hello World in Green!</h1>
+    <TodoProvider value={{todos, addTodo , updateTodo ,removeTodo , toogleComplete}}>
+
+    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+            {/* Todo form goes here */} 
+           
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+           
+        </div>
+    </div>
+    </div>
+    </TodoProvider>
 
   )
 }
